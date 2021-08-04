ec01c48f6fd674d409858bc6783662596f7dc29d
kondalapuritheja <72744143+kondalapuritheja@users.noreply.github.com>
2021-08-04 11:33:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+import LifeCycleA from './components/LifeCycleA';
+import FragmentDemo from './components/Fragment';
+import Table from './components/Table';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
+import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundry from './components/ErrorBoundry';
+function App() {
+  return (
+    <div className="App">
+     {/*<Form />*/}
+    {/*<LifeCycleA />*/}
+   {/* <FragmentDemo />  */}
+   {/*<Table />*/}
+  
+   {/*<PortalDemo />{/* <ParentComp />*/}
+   <Form />
+    </div>
+  );
+}
+
+export default App;
